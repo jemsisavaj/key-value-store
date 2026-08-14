b94c7b485826b0cdba242a25ebce9db3ce91b431
jemsisavaj <jemsisavaj08@gmail.com>
2026-08-14 06:15:12 +0530

    feat: Implement background cleanup for expired keys in HashIndex

--- include/hash_index.hpp
+++ include/hash_index.hpp
@@ -7,6 +7,9 @@
 #include <vector>
 #include <utility>
 #include <mutex>
+#include <chrono>
+#include <thread>
+#include <atomic>
 using namespace std;
 
 class HashIndex {
@@ -15,7 +18,16 @@ private:
     unordered_map<string, chrono::steady_clock::time_point> expiryStore;
     mutable mutex mtx;
 
+    //active cleanup-variable
+    atomic<bool> stopThread{false};
+    thread cleanupThread;
+
+    void cleanupExpiredKeys();
+
 public:
+    HashIndex();
+    ~HashIndex();
+    
     void put(string key, string value, int ttlInSeconds = 0);
     string get(string key);
     void del(string key);
--- src/hash_index.cpp
+++ src/hash_index.cpp
@@ -1,5 +1,41 @@
 #include "../include/hash_index.hpp"
 
+HashIndex::HashIndex(){
+    cleanupThread = thread([this](){
+        while(!stopThread){
+            this_thread::sleep_for(chrono::seconds(5));
+            this->cleanupExpiredKeys();
+        }
+    });
+}
+
+HashIndex::~HashIndex(){
+    stopThread = true;
+    if(cleanupThread.joinable()){
+        cleanupThread.join();
+    }
+}
+
+void HashIndex::cleanupExpiredKeys(){
+    lock_guard<mutex> lock(mtx);
+    auto now = chrono::steady_clock::now();
+    int count = 0;
+
+    for(auto it = expiryStore.begin(); it != expiryStore.end(); ){
+        if(now > it->second){
+            store.erase(it->first);
+            it = expiryStore.erase(it);
+            count++;
+        }
+        else{
+            ++it;
+        }
+    }
+    if(count > 0){
+        cout << "\n[Background clear] Automatically cleaned " << count << " expired keys from memory!" << endl;
+    }
+}
+
 void HashIndex::put(string key, string value, int ttlInSeconds){
     lock_guard<mutex> lock(mtx);
     store[key] = value;
